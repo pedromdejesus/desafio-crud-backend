51a18bcb78e6923d2b6f2838792185996f3a71ab
Jeej <jeejprincipal@gmail.com>
2022-06-17 10:06:12 -0300

    Criação do método post.

--- src/main/java/desafiocrud/backend/services/CryptoCurrencyService.java
+++ src/main/java/desafiocrud/backend/services/CryptoCurrencyService.java
@@ -4,6 +4,7 @@ import desafiocrud.backend.models.CryptoCurrency;
 import desafiocrud.backend.repositories.CryptoCurrencyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Date;
 import java.util.List;
 
 public class CryptoCurrencyService {
@@ -12,5 +13,10 @@ public class CryptoCurrencyService {
 
     public List<CryptoCurrency> getAllCryptoCurrencies() {
         return repository.findAll();
+    }
+
+    public CryptoCurrency postCryptoCurrency(CryptoCurrency postCC) {
+        postCC.setInclusionDate(new Date());
+        return repository.save(postCC);
     }
 }
