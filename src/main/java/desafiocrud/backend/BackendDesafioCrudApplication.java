c6e8c1c6b555d68e3d723646cb93d95dc227bc2f
Jeej <jeejprincipal@gmail.com>
2022-06-17 10:00:01 -0300

    Criação do projeto.

---
+++ src/main/java/desafiocrud/backend/BackendDesafioCrudApplication.java
@@ -0,0 +1,13 @@
+package desafiocrud.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDesafioCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDesafioCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/desafiocrud/backend/BackendDesafioCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package desafiocrud.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDesafioCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
