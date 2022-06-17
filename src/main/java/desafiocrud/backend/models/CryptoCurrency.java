2362090bb0cb5514dbbfe66f6b9380fd39fc570f
Jeej <jeejprincipal@gmail.com>
2022-06-17 10:02:09 -0300

    Criação do modelo de cripto moedas CryptoCurrency.

---
+++ src/main/java/desafiocrud/backend/models/CryptoCurrency.java
@@ -0,0 +1,25 @@
+package desafiocrud.backend.models;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name="crypto_currencies")
+public class CryptoCurrency {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String abbreviations;
+
+    private String description;
+
+    private float price;
+
+    private Date inclusionDate;
+
+    private Date updateDate;
+}
