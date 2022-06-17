ab765d494dbf1a625c0482e148b10d483634b61f
Jeej <jeejprincipal@gmail.com>
2022-06-17 10:12:31 -0300

    Atualização de annotations e criação do controller de cripto moedas CryptoCurrencyController, com todos os endpoints necessários para a API.

---
+++ src/main/java/desafiocrud/backend/controllers/CryptoCurrencyController.java
@@ -0,0 +1,34 @@
+package desafiocrud.backend.controllers;
+
+import desafiocrud.backend.models.CryptoCurrency;
+import desafiocrud.backend.services.CryptoCurrencyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/cryptocurrencies")
+@CrossOrigin(origins = "http://localhost:3000")
+public class CryptoCurrencyController {
+    @Autowired
+    private CryptoCurrencyService service;
+
+    @GetMapping
+    public List<CryptoCurrency> getAll() { return service.getAllCryptoCurrencies(); }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CryptoCurrency post(@RequestBody CryptoCurrency cc) { return service.postCryptoCurrency(cc); }
+
+    @PutMapping
+    public CryptoCurrency put(@RequestBody CryptoCurrency cc) { return service.putCryptoCurrency(cc); }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") int id) { service.deleteCryptoCurrency(id); }
+
+    @GetMapping("/search")
+    public List<CryptoCurrency> search(@RequestParam String keyword) { return service.searchCryptoCurrencies(keyword); }
+}
--- src/main/java/desafiocrud/backend/services/CryptoCurrencyService.java
+++ src/main/java/desafiocrud/backend/services/CryptoCurrencyService.java
@@ -3,11 +3,12 @@ package desafiocrud.backend.services;
 import desafiocrud.backend.models.CryptoCurrency;
 import desafiocrud.backend.repositories.CryptoCurrencyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
-
+@Service
 public class CryptoCurrencyService {
     @Autowired
     private CryptoCurrencyRepository repository;
