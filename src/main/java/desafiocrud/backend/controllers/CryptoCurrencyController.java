fda8b313e1b005c1257f58accbcfe28bf05729ae
Jeej <jeejprincipal@gmail.com>
2022-06-19 20:24:10 -0300

    Atualização do controller CryptoCurrencies para receber qualquer tipo de origem para consumo da API.

--- src/main/java/desafiocrud/backend/controllers/CryptoCurrencyController.java
+++ src/main/java/desafiocrud/backend/controllers/CryptoCurrencyController.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/cryptocurrencies")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class CryptoCurrencyController {
     @Autowired
     private CryptoCurrencyService service;
